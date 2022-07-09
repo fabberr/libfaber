8696878607d5cf2e5d2085c3af6d3cd470db9045
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-07-09 15:30:33 -0300

    removed lambdas, refactored code

--- sources/io/include/io.h
+++ sources/io/include/io.h
@@ -7,6 +7,7 @@
 #include <filesystem> 	// filesystem utilities
 #include <iostream> 	// streams
 #include <fstream> 		// file streams
+#include <string> 		// std::string
 #include <functional> 	// function objects
 #include <concepts> 	// concepts library
 
@@ -20,83 +21,58 @@ namespace faber { inline namespace v1_0_0 {
 	/** Input/output utilities and std::filesystem wrappers. */
 	namespace io {
 
+		/** Implementation details, see API section for user-facing interface. */
+		namespace impl_details {
+
+			template<typename Invocable>
+			requires std::invocable<Invocable, std::fstream&>
+			static bool read_or_write_file_impl(const std::string& filename, Invocable callback, std::ios_base::openmode mode) {
+				if (std::fstream file{filename, mode}; file.is_open()) {
+					auto ret = std::invoke(callback, file);
+					file.close();
+					return ret;
+				}
+				std::cerr << "[ERROR] Failed to open file `" << filename << "`\n";
+				return false;
+			}
+
+		} // namespace impl_details
+		
 		/********** API  **********/
 
-		/**
-		 * Implements boilerplate code for opening and closing regular files for reading
-		 * with error handling. If the file is opened successfully, calls a user-defined
-		 * function that implements all reading logic.
-		 *
-		 * @param filename Path to file for reading.
-		 * @param read_fn Function that implements the whole read operation upon an 
-		 *        already opened std::ifstream object. This function must take a 
-		 *        reference to the stream as its only parameter and return `true` if it
-		 *        succeeds in reading its contents. Note that this function (read_file)
-		 *        is responsible for opening and closing the file and, therefore, 
-		 *        `read_fn` should NOT attempt to open, close or otherwhise invalidate
-		 *        the stream.
-		 *
-		 * @returns `true` if the file could be opened and if `read_fn` returned 
-		 *          successfully, `false` otherwise.
-		*/
-		const auto read_file = [](const fs::path& filename, std::invocable<std::ifstream&> auto read_fn) -> bool {
-			bool success = false;
-			if (std::ifstream file{filename}; file.is_open()) {
-				success = read_fn(file);
-				file.close();
-			} else {
-				std::cerr << "[ERROR] Failed to open file `" << filename << "` for reading\n";
-			}
-			return success;
-		};
+		template<typename Invocable>
+		requires std::invocable<Invocable, std::fstream&>
+		bool read_file(const std::string& filename, Invocable callback, std::ios_base::openmode mode = std::ios_base::in) {
+			return impl_details::read_or_write_file_impl(filename, callback, mode |= std::ios_base::in);
+		}
 
-		/**
-		 * Implements boilerplate code for opening and closing regular files for writing
-		 * with error handling. If the file is opened successfully, calls a user-defined
-		 * function that implements all writing logic.
-		 *
-		 * @param filename Path to file for writing.
-		 * @param write_fn Function that implements the whole write operation upon an 
-		 *        already opened std::ofstream object. This function must take a 
-		 *        reference to the stream as its only parameter and return `true` if it 
-		 *        succeeds in writing its contents. Note that this function (write_file)
-		 *        is responsible for opening and closing the file and, therefore, 
-		 *        `write_fn` should NOT attempt to open, close or otherwhise invalidate 
-		 *        the stream.
-		 *
-		 * @returns `true` if the file could be opened and if `write_fn` returned 
-		 *          successfully, `false` otherwise.
-		*/
-		const auto write_fn = [](const fs::path& filename, std::invocable<std::ofstream&> auto write_fn) -> bool {
-			bool success = false;
-			if (std::ofstream file{filename}; file.is_open()) {
-				success = write_fn(file);
-				file.close();
-			} else {
-				std::cerr << "[ERROR] Failed to open file `" << filename << "` for writing\n";
-			}
-			return success;
-		};
+		template<typename Invocable>
+		requires std::invocable<Invocable, std::fstream&>
+		bool write_file(const std::string& filename, Invocable callback, std::ios_base::openmode mode = std::ios_base::out) {
+			return impl_details::read_or_write_file_impl(filename, callback, mode |= std::ios_base::out);
+		}
 
-		/**
-		 * Attempts to create a given directory. Parent directories will be created as 
-		 * needed.
-		 * If the function fails, logs an error message to stderr.
-		 *
-		 * @param dir Path to a directory.
-		 *
-		 * @returns `true` if the directory(ies) were created, `false` otherwise.
-		*/
-		const auto try_mkdir = [](const fs::path& dir) -> bool {
+		bool try_mkdir(const fs::path& dir) {
 			std::error_code ec{};
 			if (not fs::create_directories(dir, ec)) {
 				std::cerr <<
-					"[ERROR] `faber::io::try_mkdirs`: Failed to create directory(ies)\n"
+					"[ERROR] Failed to create directory(ies)\n"
 					"\tReason: " << ec.message() << "\n"
 				<< std::endl;
 				return false;
 			}
 			return true;
+		};
+
+		decltype(auto) filesize(const std::string& filename) {
+			// std::char_traits<std::fstream>::pos_type size;
+			// read_file(filename, [&size](std::fstream& file) {
+			// 	size = file.tellg();
+			// }, std::ios_base::in | std::ios_base::ate);
+			// return size;
+		};
+
+		const auto dump_file_to_stream = [](const fs::path& filename, std::ostream& os = std::cout) -> void {
 		};
 
 	} // namespace io
