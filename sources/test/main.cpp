b038a92ae9f40fc87772e2c2543ccb9e4e09c9c4
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-07-27 02:23:32 -0300

    testing file_monitor functionality

--- sources/test/main.cpp
+++ sources/test/main.cpp
@@ -1,7 +1,22 @@
-#include <io.h>
+#include <file_monitor.h>
+#include <string>
 
 int main() {
     using namespace faber;
     using namespace std::string_literals;
     using namespace std::string_view_literals;
+
+    io::file_monitor opened_files {
+        { "path/to/file1", std::fstream::in                       },
+        { "path/to/file2", (std::fstream::in | std::fstream::out) }
+    };
+
+    constexpr size_t BUF_SIZE{15};
+    const auto buf = std::make_unique<char[]>(BUF_SIZE + 1);
+    if (const auto& [stream, mode, size] = opened_files["path/to/file2"sv]; 
+        size >= BUF_SIZE && mode == (std::fstream::in | std::fstream::out))
+    {
+        stream->read(buf.get(), BUF_SIZE);
+        buf[BUF_SIZE] = '\0';
+    }
 }
