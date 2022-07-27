ec0db37f274f7ea607ee67e58371b9a1ecba5b8b
Fabricio Milanez <fabriciomilanez@protonmail.com>
2022-07-27 02:24:55 -0300

    implemented initializer list constructors, open overloads/implementation and find wrapper

--- sources/io/include/file_monitor.h
+++ sources/io/include/file_monitor.h
@@ -4,53 +4,75 @@
 /********** Headers **********/
 
 // C++ stdlib
-#include <fstream> // file streams
-#include <string_view> // std::string_view
-#include <utility> // std::pair
-#include <memory> // std:unique_ptr
-#include <unordered_map> // std::unordered_map
+#include <fstream>          // file streams
+#include <string_view>      // std::string_view
+#include <utility>          // std::pair
+#include <memory>           // std:unique_ptr
+#include <unordered_map>    // std::unordered_map
 #include <initializer_list> // std::initializer_list
+
+/********** file_monitor.h **********/
 
 namespace faber { inline namespace v1_0_0 {
 
     namespace io {
         
-        /** Manages open files. */
+        /** Manages and stores open files in a hash table. */
         class file_monitor {
         private:
             /********** Private Types **********/
 
             /** Type of initializer list for specific openmode constructor. */
-            using ilist_entry = std::pair<const std::string_view, std::fstream::openmode>;
+            using ilist_entry = std::pair<const char*, std::fstream::openmode>;
 
-            /** Mapped-to type of hash map. Destructuring is supported. */
+            /** Mapped type of hash table. Destructuring is supported. */
             struct file_info {
-                std::unique_ptr<std::fstream> 	stream = nullptr;
-                std::fstream::openmode 			mode = static_cast<std::fstream::openmode>(0);
-                std::uint32_t 					size = 0;
-                /** @todo add a timestap maybe? */
+
+                /********** RAII **********/
+                
+                file_info(
+                    std::unique_ptr<std::fstream>&& stream  = nullptr, 
+                    std::fstream::openmode          mode    = static_cast<std::fstream::openmode>(0), 
+                    std::uint32_t                   size    = 0U
+                ) : stream(std::move(stream)), mode(mode), size(size) { }
+                
+                /********** Rule of Five **********/
+
+                file_info(const file_info&) = delete;
+                file_info(file_info&&)      = delete;
+                ~file_info()                = default;
+
+                file_info& operator=(const file_info&)  = delete;
+                file_info& operator=(file_info&&)       = delete;
+
+                /********** Public Members **********/
+                
+                std::unique_ptr<std::fstream>   stream; /** Pointer to an open file stream on the heap. */
+                std::fstream::openmode          mode;   /** Mode which the stream was opened with. */
+                std::uint32_t                   size;   /** Size (in byes) of the file associated to the stream. */
+                /** @todo add a timestamp of when the file was opened maybe? */
             };
 
             /**
              * The underlying data structure.
-             * A hash map that associates an unique filename to an object containing a 
+             * A hash table that maps an unique filename to an object containing a 
              * `std::fstream` associated to it and other pertinent info.
             */
-            using hash_map = std::unordered_map<std::string_view, file_info>;
+            using hashtable = std::unordered_map<std::string_view, file_info>;
         
         public:
             /********** Forward Declared Public Types **********/
             
-            typedef file_info   file_info_t;
-            typedef hash_map    hash_map_t;
+            typedef file_info file_info_t;
+            typedef hashtable hashtable_t;
 
         public:
             /********** Constructors & Destructor **********/
             
-            file_monitor() = default; // default-initializes members
+            file_monitor() = default; // default constructor
 
-            file_monitor(const std::initializer_list<const std::string_view>&&); 	/** opens 1..* files (in|out) */
-            file_monitor(const std::initializer_list<ilist_entry>&&); 				// opens 1..* files, specify openmode for each
+            file_monitor(std::initializer_list<const char*> ilist); // opens 1..* files (in|out)
+            file_monitor(std::initializer_list<ilist_entry> ilist); // opens 1..* files, specify openmode for each
 
             file_monitor(const file_monitor&) 	= delete; // copy constructor (deleted)
             file_monitor(const file_monitor&&) 	= delete; // move constructor (deleted)
@@ -60,16 +82,17 @@ namespace faber { inline namespace v1_0_0 {
         private:
             /********** Private Member Functions **********/
 
-            std::fstream& open_impl(std::string_view filename, std::fstream::openmode mode);
+            std::fstream& open_impl(const char* filename, std::fstream::openmode mode);
 
         public:
             /********** Public Member Functions **********/
 
-            std::fstream& open(std::string_view filename); // open file (in|out) and register its stream on the map
-            std::fstream& open(std::string_view filename, std::fstream::openmode mode); // open file (specify openmode)
-            void close(std::string_view filename); // close file and remove its stream from the map
+            std::fstream& open(const char* filename); // open file (in|out) and register its stream on the table
+            std::fstream& open(const char* filename, std::fstream::openmode mode); // open file (specify openmode)
+            void close(std::string_view filename); // close file and remove its stream from the table
 
-            hash_map_t::iterator find(std::string_view filename) const;	// wrapper for unordered_map::find
+            /** Wrapper for unordered_map::find (const version only). */
+            hashtable_t::const_iterator find(std::string_view filename) const;
             
             /** Wrapper for std::unordered_map::at. */
             const file_info_t& operator[](const std::string_view key) const;
@@ -77,8 +100,8 @@ namespace faber { inline namespace v1_0_0 {
             /** Wrapper for std::unordered_map::operator[]. */
             file_info_t& operator[](const std::string_view key);
 
-            const hash_map_t& opened_files() const; // return const reference to underlying data structure, non-const version below
-            hash_map_t& opened_files(); // warning: calling operator[] on this reference may insert a new default-constructed fstream not associeated with any file
+            const hashtable_t& data() const; // return const reference to underlying data structure, non-const version below
+            hashtable_t& data();
             
             file_monitor& operator=(const file_monitor&)    = delete; // copy assignment (deleted)
             file_monitor& operator=(file_monitor&&)         = delete; // move assignment (deleted)
@@ -86,7 +109,7 @@ namespace faber { inline namespace v1_0_0 {
         private:
             /********** Private Members **********/
 
-            hash_map_t _opened_files{};
+            hashtable_t _opened_files{};
         }; // class file_monitor
         
     } // namespace io
@@ -96,4 +119,5 @@ namespace faber { inline namespace v1_0_0 {
 
 #endif // FABER_FILE_MONITOR_H
 
+/** @todo make file_monitor a singleton */
 /** @todo accessors for ios_base::openmode */
--- sources/io/src/file_monitor.cpp
+++ sources/io/src/file_monitor.cpp
@@ -1,50 +1,89 @@
 /********** Headers **********/
 
+// C++ stdlib
+#include <iostream>     // std::cerr
+#include <stdexcept>    // std::runtime_error
+
+// C stdlib
+#include <cstring> // std::memcpy
+
 // internal
 #include <file_monitor.h>
+#include <io.h>
 
 using file_monitor  = faber::io::file_monitor;
-using hash_map_t    = file_monitor::hash_map_t;
+using hashtable_t   = file_monitor::hashtable_t;
 using file_info_t   = file_monitor::file_info_t;
 
 /********** file_monitor.cpp **********/
 
 /********** Constructors & Destructor **********/
 
-file_monitor::file_monitor(const std::initializer_list<const std::string_view>&&) {
+file_monitor::file_monitor(std::initializer_list<const char*> ilist) {
+    for (const auto& entry : ilist) {
+        open(entry);
+    }
 }
 
-file_monitor::file_monitor(const std::initializer_list<ilist_entry>&&) {
+file_monitor::file_monitor(std::initializer_list<ilist_entry> ilist) {
+    for (const auto& [filename, mode] : ilist) {
+        open(filename, mode);
+    }
 }
 
 /********** Private Member Functions **********/
 
-std::fstream& file_monitor::open_impl(std::string_view filename, std::fstream::openmode mode) {
+std::fstream& file_monitor::open_impl(const char* filename, std::fstream::openmode mode) {
+
+    if (const auto it = find(filename); it != _opened_files.cend()) {
+        throw std::runtime_error{"error: file already opened on monitor."};
+    }
+    
+    auto f_ptr = std::make_unique<std::fstream>(filename, mode);
+    if (not *f_ptr) {
+        throw std::runtime_error{"error: could not open file"};
+    }
+
+    // Transfer ownership of pointer to a file_info_t temporary which will be constructed
+    // in place as a new value in the hash table with `filename` as its key.
+    // `std::unordered_map::emplace` returns a pair containing an iterator to the possibly
+    // newly inserted kv-pair and a boolean. This boolean will be set to true if the value
+    // was actually inserted, and false if the key was already present.
+    // Values `inserted` and `key` below are unused but required for destructuring.
+    [[maybe_unused]] const auto& [iterator, inserted] = _opened_files.emplace(filename, file_info_t{ std::move(f_ptr), mode, faber::io::filesize(*f_ptr) });
+    [[maybe_unused]] const auto& [key, info] = *iterator;
+    return *info.stream;
+
+    // The alternative is this cursed one-liner with poor readability. Performance losses are practically nonexistent.
+    // return *_opened_files.emplace(filename, file_info_t{ std::move(f_ptr), mode, faber::io::filesize(*f_ptr) }).first->second.stream;
 }
 
 /********** Public Member Functions **********/
 
-std::fstream& file_monitor::open(std::string_view filename) {
+std::fstream& file_monitor::open(const char* filename) {
+    return open_impl(filename, (std::fstream::in | std::fstream::out));
 }
 
-std::fstream& file_monitor::open(std::string_view filename, std::fstream::openmode mode) {
+std::fstream& file_monitor::open(const char* filename, std::fstream::openmode mode) {
+    return open_impl(filename, mode);
 }
 
 void file_monitor::close(std::string_view filename) {
 }
 
-hash_map_t::iterator file_monitor::find(std::string_view filename) const {
+hashtable_t::const_iterator file_monitor::find(std::string_view filename) const {
+    return _opened_files.find(filename);
 }
 
 const file_info_t& file_monitor::operator[](const std::string_view key) const {
+    return _opened_files.at(key);
 }
 
-/** This description does not show up, IntelliSense prefers the display whatever comments it finds on the file that declares this. */
 file_info_t& file_monitor::operator[](const std::string_view key) {
 }
 
-const hash_map_t& file_monitor::opened_files() const {
+const hashtable_t& file_monitor::data() const {
 }
 
-hash_map_t& file_monitor::opened_files() {
+hashtable_t& file_monitor::data() {
 }
